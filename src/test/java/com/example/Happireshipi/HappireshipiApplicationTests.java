8efc9ee8c8152202696c04434cc95aea0c53bd05
Nomuron <Nomuron@gmail.com>
2022-11-11 16:24:46 +0100

    Added base Spring web project

---
+++ src/test/java/com/example/Happireshipi/HappireshipiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Happireshipi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappireshipiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
