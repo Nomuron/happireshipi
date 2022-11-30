cab058e0999d0e779ecd3a7c2d1814e0bf501b68
s22051-pj-lukasz-kos <s22051@pjwstk.edu.pl>
2022-11-30 18:37:20 +0100

    application.properties changed
    Spring environment set in docker-compose

--- src/main/java/com/example/Happireshipi/repository/MealRepository.java
+++ src/main/java/com/example/Happireshipi/repository/MealRepository.java
@@ -1,9 +1,10 @@
 package com.example.Happireshipi.repository;
 
 import com.example.Happireshipi.dao.Meal;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MealRepository extends CrudRepository<Meal, Integer> {
+public interface MealRepository extends JpaRepository<Meal, Integer> {
 }
